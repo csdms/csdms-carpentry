2987cb82da0b5159c743eee9404cefa4064fdff9
Mark Piper <mark.piper@colorado.edu>
2016-05-10 15:57:58 -0600

    Add PBS batch script examples

---
+++ lessons/shell/code/calculate_pi.pbs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# A PBS script that calls a Python script that calculates an
+# approximation to pi. Submit this script to the queue manager with:
+#
+#  $ qsub calculate_pi.pbs.sh
+
+echo "Calculating pi with the Bailey–Borwein–Plouffe formula"
+echo "Start time:" `date`
+python calculate_pi.py 20
+echo "End time:" `date`
---
+++ lessons/shell/code/simple.pbs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# A simple PBS script. Submit this script to the queue manager with:
+#
+#  $ qsub simple.pbs.sh
+
+echo "Hello world!"
